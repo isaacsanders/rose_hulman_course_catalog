eadc6d0c9bc593dd6d78a3f5fba212550511790c
Isaac Sanders <isaac@isaacbfsanders.com>
2012-07-24 22:28:05 -0400

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoseHulmanCourseCatalog::Application.config.secret_token = '8330660e79a3b293f92377c3eea372acf06369f64ea4962833ec63f682f4eb609cff2b9edf3a9e7e45acaa7474f855cef28ac57f9a6cc1570d08c2eaa20405cd'
